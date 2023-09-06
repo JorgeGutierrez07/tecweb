0072e7e77655640dbdf54b6381b716917884c9a1
JorgeGutierrez <jorgeandresgutierrezcortes07@gmail.com>
2023-09-05 18:22:52 -0600

    Ejercicio 2 incisos diversos completado

--- practicas/p03/Ejercicio2.php
+++ practicas/p03/Ejercicio2.php
@@ -3,12 +3,16 @@
     $a = "ManejadorSQL";
     $b = 'MySQL';
     $c = &$a; 
-    echo 'Variables del Ejercicio 2';
+    echo '<p>Variables del Ejercicio 2';
+    echo '<p>Iniciso d) </p>';
+    echo '<p>Se nombraron variables dentro de la pagina y la variable de $a cambia a los </p>';
+    echo '<p>y $b hace referencia al valor de $a</p>';
     echo '<br>';
-    echo 'Contenido de variable de $a:  ',$a;
-    echo '<br>';
-    echo 'Contenido de variable de $b:  ',$b;
-    echo '<br>';
-    echo 'Contenido de variable de $c:  ',$c;
-    echo '<br>';
+    echo '<p>Contenido de variable de $a:  </p>',$a;
+    echo '<p>Contenido de variable de $b:  </p>',$b;
+    echo '<p>Contenido de variable de $c:  </p>',$c;
+
+    $a = "PHP server";
+    $b = &$a;
+    echo '<p>Variable $b y $a modificada:  </p>',$b;
 ?>
