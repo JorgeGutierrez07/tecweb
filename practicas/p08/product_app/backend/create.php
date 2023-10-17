6b27dbb8da98ae6ac7651d97e61971cc96571c10
JorgeGutierrez <jorgeandresgutierrezcortes07@gmail.com>
2023-10-16 21:06:54 -0600

    Punto 3 corregido

--- practicas/p08/product_app/backend/create.php
+++ practicas/p08/product_app/backend/create.php
@@ -1,36 +1,33 @@
 <?php
 include_once __DIR__ . '/database.php';
-
+$ver = ["mensaje" => ""];
 // SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
 $producto = file_get_contents('php://input');
 if (!empty($producto)) {
     // SE TRANSFORMA EL STRING DEL JASON A OBJETO
     $jsonOBJ = json_decode($producto);
-    /**
-     * SUSTITUYE LA SIGUIENTE LÍNEA POR EL CÓDIGO QUE REALICE
-     * LA INSERCIÓN A LA BASE DE DATOS. COMO RESPUESTA REGRESA
-     * UN MENSAJE DE ÉXITO O DE ERROR, SEGÚN SEA EL CASO.
-     */
-    $ver = ["mensaje" => ""];
-    $checaObjeto = $conexion->query("SELECT COUNT(*) FROM productos WHERE nombre='{$jsonOBJ->nombre}' AND eliminado=0");
-    $checaObjeto1 = $conexion->query("SELECT COUNT(*) FROM productos WHERE nombre='{$jsonOBJ->nombre}' AND eliminado=1");
-    if ($checaObjeto->num_rows == 0) {
-        $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
+    //SE EVALUA SI EL PRODUCTO YA EXISTE EN LA BASE DE DATOS
+    $checaobjeto = $conexion->query("SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND eliminado = 0 ");
+    $checaobjeto1 = $conexion->query("SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND eliminado = 1");
+
+    if ($checaobjeto->num_rows == 0) {
+        $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}',0)";
         if ($conexion->query($sql)) {
-            $ver["mensaje"] = "Producto agregado con exito con ID: " . $conexion->insert_id;
+            $ver["mensaje"] = "Producto registrado con ID: ".$conexion->insert_id;
         } else {
-            $ver["mensaje"] = "Producto no agregado";
+            $ver["mensaje"] = "Producto no registrado";
         }
-    } elseif ($checaObjeto1->num_rows == 1) {
-        $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
+    } elseif ($checaobjeto1->num_rows == 1) {
+        $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}',0)";
         if ($conexion->query($sql)) {
-            $ver["mensaje"] = "Producto existente pero vigente agregado con exito  : " . $conexion->insert_id;
+            $ver["mensaje"] = "El producto existente en la base de datos y vigente".$conexion->insert_id;
         } else {
-            $ver["mensaje"] = "Producto no agregado";
+            $ver["mensaje"] = "Producto no registrado ";
         }
     } else {
-        echo 'no agregado';
+        $ver["mensaje"] = "Producto disponible y vigiente por lo tanto no puede ser agregado";
     }
-    $conexion->close();
 }
+    $conexion->close();
+
 echo json_encode($ver, JSON_PRETTY_PRINT);
